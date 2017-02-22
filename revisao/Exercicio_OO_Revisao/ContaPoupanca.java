91f1107121a41b7fefb47bb3d4ea7c0245b1fc19
phoenixproject <phoenixproject.erp@gmail.com>
2017-02-22 10:20:07 -0300

    Create ContaPoupanca.java

---
+++ revisao/Exercicio_OO_Revisao/ContaPoupanca.java
@@ -0,0 +1,11 @@
+public class ContaPoupanca extends Conta{
+    private Double juros;
+    
+    public Double getJuros(){
+        return this.juros;
+    }
+    
+    public void setJuros(Double juros){
+        this.juros = juros;
+    }
+}
