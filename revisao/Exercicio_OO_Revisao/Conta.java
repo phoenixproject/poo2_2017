ebfe55b46e30770f5baeb42ed1ebdbaeb009894c
phoenixproject <phoenixproject.erp@gmail.com>
2017-02-22 10:08:45 -0300

    Create Conta.java

---
+++ revisao/Exercicio_OO_Revisao/Conta.java
@@ -0,0 +1,11 @@
+public class Conta{
+   private Double saldo;
+   
+   public getSaldo(){
+      return  this.saldo;
+   }
+   
+   public setSaldo(Double saldo){
+      this.saldo = saldo;
+   }
+}
