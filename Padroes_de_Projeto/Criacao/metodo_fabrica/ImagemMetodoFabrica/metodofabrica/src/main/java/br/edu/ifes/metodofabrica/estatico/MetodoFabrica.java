71a3eda136f9481d1d42fa88042adaafaef0f655
20101bsi0453 <20101bsi0453@SR4584.cefetes.br>
2017-03-07 09:07:10 -0300

    Arquivos de Gerador de Gif e Bmp adicionados.

--- Padroes_de_Projeto/Criacao/metodo_fabrica/ImagemMetodoFabrica/metodofabrica/src/main/java/br/edu/ifes/metodofabrica/estatico/MetodoFabrica.java
+++ Padroes_de_Projeto/Criacao/metodo_fabrica/ImagemMetodoFabrica/metodofabrica/src/main/java/br/edu/ifes/metodofabrica/estatico/MetodoFabrica.java
@@ -13,10 +13,17 @@ public class MetodoFabrica {
     
     public static void main(String[] args) {
         Formato formato;
+        
         formato = FabricaFormato.geraFormato("metodoestatico.fabrica.FormatoPng");
         formato.gera();
         
         formato = FabricaFormato.geraFormato("metodoestatico.fabrica.FormatoJpeg");
+        formato.gera();
+        
+        formato = FabricaFormato.geraFormato("metodoestatico.fabrica.FormatoGif");
+        formato.gera();
+        
+        formato = FabricaFormato.geraFormato("metodoestatico.fabrica.FormatoBmp");
         formato.gera();
     }      
     
