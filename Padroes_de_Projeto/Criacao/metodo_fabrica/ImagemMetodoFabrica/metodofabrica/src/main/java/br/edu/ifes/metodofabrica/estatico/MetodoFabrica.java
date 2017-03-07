ad9572401eab36bf113daa222a69238db7b519e9
20101bsi0453 <20101bsi0453@SR4584.cefetes.br>
2017-03-07 09:01:58 -0300

    Arquivo do Projeto Maven adicionados no projeto.

---
+++ Padroes_de_Projeto/Criacao/metodo_fabrica/ImagemMetodoFabrica/metodofabrica/src/main/java/br/edu/ifes/metodofabrica/estatico/MetodoFabrica.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifes.metodofabrica.estatico;
+
+/**
+ *
+ * @author 20101bsi0453
+ */
+public class MetodoFabrica {
+    
+    public static void main(String[] args) {
+        Formato formato;
+        formato = FabricaFormato.geraFormato("metodoestatico.fabrica.FormatoPng");
+        formato.gera();
+        
+        formato = FabricaFormato.geraFormato("metodoestatico.fabrica.FormatoJpeg");
+        formato.gera();
+    }      
+    
+}
