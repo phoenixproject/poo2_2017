95b6f9f9fd93e44950f1f595f657bbb796ac0d90
Phoenix Project <phoenixproject.erp@gmail.com>
2017-03-14 09:01:51 -0300

    Alterado localização de método fábrica de imagem.

---
+++ Padroes_de_Projeto/Criacao/metodo_fabrica/ImagemMetodoFabrica/imagem_metodo_fabrica/src/main/java/br/edu/ifes/metodofabrica/estatico/FormatoJpeg.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifes.metodofabrica.estatico;
+
+/**
+ *
+ * @author 20101bsi0453
+ */
+public class FormatoJpeg implements Formato {
+    
+    @Override
+    public Imagem gera() {
+        System.out.println("gera jpeg");
+        return null;
+    }
+}
